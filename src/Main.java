4f3152b87ce4712fc287827e74b6b6102882d2fb
nikonovdmitiy <nikonov9094@gmail.com>
2022-08-12 15:38:02 +0500

    дз

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,35 @@ public class Main {
 
         //1 задача
         //1
-        int [] a = {1, 2, 3};
-        for (int i = 0; i<3; i++) {
-            if (i==2){
-                System.out.print(a[i]+" ");
+        int[] a = {1, 2, 3};
+        for (int i = 0; i < 3; i++) {
+            if (i == 2) {
+                System.out.print(a[i] + " ");
             } else {
-
-
-                System.out.print(a[i]+",");
+                System.out.print(a[i] + ", ");
             }
         }
 
         //2
-        double [] b = new double[]{1.57, 7.654, 9.986};
+        System.out.println("\n");
+        double [] b = {1.57, 7.654, 9.986};
         for (double u = 0.0; u < 3.0; u++) {
-            System.out.print(b[(int) u] + ",");
+            if (u == 2) {
+                System.out.print(b[(int) u] + " ");
+            } else {
+                System.out.print(b[(int) u] + ", ");
+            }
         }
 
         //3
+        System.out.println("\n");
         int [] c = {1, 2, 3, 4, 5, 6, 7};
         for (int y = 0; y < 7; y++) {
-            System.out.print(c[y]+",");
+            if (y == 6) {
+                System.out.print(c[y] + " ");
+            } else {
+                System.out.print(c[y] + ", ");
+            }
         }
 
 
@@ -32,4 +40,6 @@ public class Main {
 
 
 
+        }
     }
+
